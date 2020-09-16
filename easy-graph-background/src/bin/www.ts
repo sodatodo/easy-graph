332fe7a5c2e0024b25cce2044100a4bf011cf501
soda <wangsuda@icloud.com>
2020-09-16 22:33:59 +0800

    feat: add graphql server

--- easy-graph-background/src/bin/www.ts
+++ easy-graph-background/src/bin/www.ts
@@ -1,1 +1,24 @@
-console.log('Hello world');
+const Koa = require('koa');
+const { ApolloServer, gql } = require('apollo-server-koa');
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = new Koa();
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () => {
+  console.log('server ready at http://localhost:4000');
+});
